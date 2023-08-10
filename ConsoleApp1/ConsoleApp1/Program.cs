55f9f73378413870e8a75bdd6e501dbffcdced5a
fidan <fidanji@code.edu.az>
2023-08-10 15:27:25 +0400

    GetCard

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,8 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Adeliye");
+            var getCard = new GetCard();
+            getCard.CarrierName = "Bakcell";
+            getCard.Number = "+994554242979";
+            getCard.Balance = 10;
+            getCard.Tariff = 0.5m;
+
+            Console.WriteLine(getCard.CarrierName);
+            Console.WriteLine(getCard.Number);  
+            Console.WriteLine(getCard.Balance); 
+            Console.WriteLine(getCard.Tariff);
+
+            getCard.IncreaseBalance(30.5m);
+            Console.WriteLine(getCard.Balance);
+
+            if (getCard.Call ())
+            {
+                Console.WriteLine("You can make a call!");
+            }
+            else
+            {
+                Console.WriteLine("not enough balance");
+            }
+        }
+    }
+
+
+    public class GetCard
+    {
+        public string CarrierName { get; set; }
+        public string Number { get; set; }
+        public decimal Balance { get; set; }
+        public decimal Tariff { get; set; }
+
+        public void IncreaseBalance(decimal amount)
+        {
+            if (amount <= 0) { return; }
+
+            Balance += amount;
+        }
+
+        public bool Call()
+        {
+            if (Balance > Tariff)
+            { 
+                Balance -= Tariff;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
